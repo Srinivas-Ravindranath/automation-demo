b422c108cac6fcd626bdf5d03c090bb52e57d0fe
Srinivas Ravindranath <srinivasravindranath1997@gmail.com>
2024-03-01 22:17:21 -0500

    fix image_id

--- terraform/asg.tf
+++ terraform/asg.tf
@@ -91,7 +91,7 @@ data "aws_ami" "ubuntu" {
 
 resource "aws_launch_template" "web_server_template" {
   name_prefix   = var.launch_template_prefix
-  image_id      = "ami-0eb45246cba02871b"
+  image_id      = data.aws_ami.ubuntu.image_id
   instance_type = var.instance_type
 
   block_device_mappings {
