15c444ea94584c2411a10ade0e0904cdc482118c
Srinivas Ravindranath <srinivasravindranath1997@gmail.com>
2024-12-04 22:23:44 -0500

    fix backend

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "cloud-computing-6907-81-terraform-state-bucket"
+    bucket = "cloud-computing-6907-81-terraform-state-bucket-new"
     key    = "demo_deployment/terraform.tfstate"
     region = "us-east-1"
   }
