dacb1da859dc38f1d640c48f0f820400c5c4722b
Srinivas R <intervention@Srinivass-Mac-mini.local>
2024-12-04 22:09:32 -0500

    change bucket name

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 # Configure the S3 bucket to store the Terraform state file
 terraform {
   backend "s3" {
-    bucket = "cloud-computing-6907-81-terraform-state-bucket"
+    bucket = "cloud-computing-6907-81-terraform-state-bucket-new"
     key    = "demo_deployment/terraform.tfstate"
     region = "us-east-1"
   }
